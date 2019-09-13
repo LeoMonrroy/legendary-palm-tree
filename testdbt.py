aa468ab27935b6920fb6ac8aa88087ec0f6895bf
LeoMonrroy <55181613+LeoMonrroy@users.noreply.github.com>
2019-09-13 11:12:11 +0200

    Update testdbt.py

--- testdbt.py
+++ testdbt.py
@@ -1,3 +1,28 @@
+    
+# Import required libraries
+import os
+from random import randint
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+import flask
+import dash
+from dash.dependencies import Input, Output, State, Event
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
+
+# Put your Dash code here
+
 # -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
@@ -28,5 +53,6 @@ app.layout = html.Div(children=[
     )
 ])
 
+# Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.server.run(debug=True, threaded=True)
