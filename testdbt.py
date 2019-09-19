37e843dc9c984644cb1b5fc3d9dd8b5ce711eaa1
LeoMonrroy <55181613+LeoMonrroy@users.noreply.github.com>
2019-09-19 12:15:23 +0200

    Update testdbt.py

--- testdbt.py
+++ testdbt.py
@@ -18,8 +18,8 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3, 4, 5], 'y': [4, 1, 2, 4, 7], 'type': 'dot', 'name': 'T1'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'dot', 'name': u'T2'},
+                {'x': [1, 2, 3, 4, 5], 'y': [4, 1, 2, 4, 7], 'type': 'line', 'name': 'T1'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'dot', 'line': u'T2'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
